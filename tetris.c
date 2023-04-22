1526144deaca122a8796d10afe053053513ba0cb
Cyanea <g3tsuzumi@gmail.com>
2023-04-23 02:37:24 +0900

    update

--- tetris.c
+++ tetris.c
@@ -1,11 +1,117 @@
-//#include <curses.h>
+#include <ncurses.h>
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 //フィールドサイズ
 #define FIELD_WIDTH     (10)
 #define FIELD_HEIGHT    (20)
 
-void DrowScreen() {
+//テトリミノの情報
+#define SHAPE_WIDTH_MAX     (4)
+#define SHAPE_HEIGHT_MAX    (4)
+enum {
+    SHAPE_I,
+    SHAPE_O,
+    SHAPE_S,
+    SHAPE_Z,
+    SHAPE_J,
+    SHAPE_L,
+    SHAPE_T,
+    SHAPE_MAX
+};
+typedef struct {
+    int width, height;
+    int pattern[SHAPE_HEIGHT_MAX][SHAPE_WIDTH_MAX];
+}SHAPE;
+
+typedef struct {
+    int x, y;
+    SHAPE shape;
+}MINO;
+
+SHAPE shapes[SHAPE_MAX] = {
+    // SHAPE_I
+    {
+        4,4,    // int width, height;
+    // int pattern[SHAPE_HEIGHT_MAX][SHAPE_WIDTH_MAX];
+        {
+            {0,0,0,0},
+            {1,1,1,1},
+            {0,0,0,0},
+            {0,0,0,0}
+        }
+    },
+    // SHAPE_O
+    {
+        4,4,    // int width, height;
+    // int pattern[SHAPE_HEIGHT_MAX][SHAPE_WIDTH_MAX];
+        {
+            {1,1},
+            {1,1},
+        }
+    },
+
+    // SHAPE_S
+    {
+        3,3,    // int width, height;
+    // int pattern[SHAPE_HEIGHT_MAX][SHAPE_WIDTH_MAX];
+        {
+            {0,1,1},
+            {1,1,0},
+            {0,0,0}
+        }
+    },
+
+    // SHAPE_Z
+    {
+        3,3,    // int width, height;
+    // int pattern[SHAPE_HEIGHT_MAX][SHAPE_WIDTH_MAX];
+        {
+            {1,1,0},
+            {0,1,1},
+            {0,0,0}
+        }
+    },
+
+    // SHAPE_J
+    {
+        3,3,    // int width, height;
+    // int pattern[SHAPE_HEIGHT_MAX][SHAPE_WIDTH_MAX];
+        {
+            {1,0,0},
+            {1,1,1},
+            {0,0,0}
+        }
+    },
+
+    // SHAPE_L
+    {
+        3,3,    // int width, height;
+    // int pattern[SHAPE_HEIGHT_MAX][SHAPE_WIDTH_MAX];
+        {
+            {0,0,1},
+            {1,1,1},
+            {0,0,0}
+        }
+    },
+
+    // SHAPE_T
+        {
+        3,3,    // int width, height;
+    // int pattern[SHAPE_HEIGHT_MAX][SHAPE_WIDTH_MAX];
+        {
+            {0,1,0},
+            {1,1,1},
+            {0,0,0}
+        }
+    },
+};
+
+int field[FIELD_HEIGHT][FIELD_WIDTH];
+MINO mino;
+
+void DrawScreen() {
     for(int y=0; y< FIELD_HEIGHT; y++) {
         printf("□");
             for(int x=0; x < FIELD_WIDTH; x++)
@@ -17,6 +123,12 @@ void DrowScreen() {
                 printf("□");
         }
 
+void Init() {
+    mino.shape = shapes[rand() % SHAPE_MAX];
+    DrawScreen();
+}
+
 int main(){
-    DrowScreen();
+    srand((unsigned int)time(NULL));
+    Init();
 }
