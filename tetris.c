daced980a3380db30d5fb7e102dd2a11c0ab114c
Cyanea <g3tsuzumi@gmail.com>
2023-04-25 00:14:31 +0900

    update

--- tetris.c
+++ tetris.c
@@ -1,5 +1,6 @@
 #include <ncurses.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include <stdio.h>
 
@@ -44,7 +45,7 @@ SHAPE shapes[SHAPE_MAX] = {
     },
     // SHAPE_O
     {
-        4,4,    // int width, height;
+        2,2,    // int width, height;
     // int pattern[SHAPE_HEIGHT_MAX][SHAPE_WIDTH_MAX];
         {
             {1,1},
@@ -109,13 +110,26 @@ SHAPE shapes[SHAPE_MAX] = {
 };
 
 int field[FIELD_HEIGHT][FIELD_WIDTH];
+int screen[FIELD_HEIGHT][FIELD_WIDTH];
 MINO mino;
 
+bool MinoIntersectField() {
+
+}
+
 void DrawScreen() {
+    memcpy(screen, field, sizeof field);
+
+    for(int y=0; y<mino.shape.height; y++)
+        for(int x=0; x<mino.shape.width; x++)
+            if(mino.shape.pattern[y][x])
+                screen[mino.y + y][mino.x + x] |= 1;
+system("cls");
+
     for(int y=0; y< FIELD_HEIGHT; y++) {
         printf("□");
             for(int x=0; x < FIELD_WIDTH; x++)
-                printf(" ");
+                printf("%s", screen[y][x] ? "■" : " ");
             printf("□");
             printf("\n");
             }
@@ -123,12 +137,43 @@ void DrawScreen() {
                 printf("□");
         }
 
-void Init() {
+void InitMino() {
     mino.shape = shapes[rand() % SHAPE_MAX];
+    mino.x = (FIELD_WIDTH - mino.shape.width) / 2;
+    mino.y = 0;
+}
+
+void Init() {
+    memset(field, 0, sizeof field);
+    InitMino();
     DrawScreen();
 }
 
 int main(){
     srand((unsigned int)time(NULL));
     Init();
+    //ゲームのループ
+    while(1) {
+        if() {
+            switch () {
+            case 'w':
+                break;
+
+            case 'a':
+            mino.x--;
+                break;
+
+            case 's':
+            mino.y++;
+                break;
+
+            case 'd':
+            mino.x++;
+                break;
+            
+            default:
+                break;
+            }
+        }
+    }
 }
